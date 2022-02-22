8cbcdb7ad8e5f25a54d0bccbcbd4554888b4fba2
AungZawHtet <aungzawhteth10@gmail.com>
2022-02-23 01:08:44 +0900

    change dir of index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require __DIR__ . '/bootstrap/app.php';
+require __DIR__ . '/src/app/ui/routes_ui.php';
+$app->run();
--- public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__ . '/../bootstrap/app.php';
-require __DIR__ . '/../src/app/ui/routes_ui.php';
-$app->run();
