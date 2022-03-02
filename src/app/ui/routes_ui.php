3c800424cdbe6312a3793622b9a31a0c9c11d766
AungZawHtet <aungzawhteth10@gmail.com>
2022-03-02 21:12:57 +0900

    rebuild project

--- src/app/ui/routes_ui.php
+++ src/app/ui/routes_ui.php
@@ -1,7 +1,7 @@
 <?php
-$app->get('/', function ($request, $response) {
-    return 'index';
-});
 $app->get('/{id}', function ($request, $response, $args) {
     return $args['id'];
 });
+// $app->get('/', function ($request, $response) {
+//     return 'index';
+// });
