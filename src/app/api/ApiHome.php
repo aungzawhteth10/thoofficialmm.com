8c289fc045ea543d7efb68c63fa2948b6534c277
AungZawHtet <aungzawhteth10@gmail.com>
2022-03-08 22:34:28 +0900

    admin to adminHome

--- src/app/api/ApiHome.php
+++ src/app/api/ApiHome.php
@@ -8,9 +8,11 @@ class ApiHome extends ApiBase
         $jsonFilePath = getcwd() . '/src/app/files/product.json';
         $productArr = json_decode(file_get_contents($jsonFilePath), TRUE);
         foreach ($productArr as $key => $value) {
+            $images = explode(',', $value['images']);
+            $image = ($images[0] == '') ? 'logo.png' : $images[0];
             $items[] = [
                 'product_code' => $value['product_code'],
-                'image'        => (explode(',', $value['images'])[0] ?? 'logo.png') . '?' . time(),
+                'image'        => $image . '?' . time(),
                 'name'         => $value['name'],
                 'price'        => $value['price'],
                 'avablility'   => $value['avablility'],
